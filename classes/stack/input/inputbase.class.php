c359d25feba9de86524ab40197fbd181cb45792f
msalterhslu <mark.salter@hslu.ch>
2023-11-15 10:18:18 +0100

    TEMP PATCH HSLU: The validation of student response with NumAbsolute or NumRelative failed because simplify set to false

--- classes/stack/input/inputbase.class.php
+++ classes/stack/input/inputbase.class.php
@@ -650,7 +650,6 @@ abstract class stack_input {
             throw new stack_exception('stack_input: validate_student_response: options not of class stack_options');
         }
         $localoptions = clone $options;
-        $localoptions->set_option('simplify', false);
         if ($this->get_extra_option('simp')) {
             $localoptions->set_option('simplify', true);
         }
